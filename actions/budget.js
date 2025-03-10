81a7d2b681ced19a68eb638dcc0415ba41691121
stupidman111 <2456306700@qq.com>
2025-03-10 17:42:46 +0800

    dashboard: monthly budget

---
+++ actions/budget.js
@@ -0,0 +1,107 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function getCurrentBudaget(accountId) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // 查找用户的预算
+    const budget = await db.budget.findFirst({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    // 计算当前月份的起始时间和结束时间
+    /**
+     * Eg：
+     * const currentDate = new Date(2025, 2, 10); // 2025-03-10
+     * const startOfMonth = new Date(2025, 2, 1); // 2025-03-01
+     * const endOfMonth = new Date(2025, 3, 0);  // 2025-03-31
+     */
+    const currentDate = new Date();
+    const startOfMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      1
+    );
+    const endOfMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      0
+    );
+
+    // 查询当前用户在本月的所有支出总额
+    const expenses = await db.transaction.aggregate({
+      where: {
+        userId: user.id,
+        type: "EXPENSE",
+        date: {
+          gte: startOfMonth, //great than
+          lte: endOfMonth, //less than
+        },
+        accountId,
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    // 返回预算信息和当前月份支出金额
+    return {
+      budget: budget ? { ...budget, amount: budget.amount.toNumber() } : null,
+      currentExpenses: expenses._sum.amount
+        ? expenses._sum.amount.toNumber()
+        : 0,
+    };
+  } catch (error) {
+    console.error(error);
+    return { error: error.message };
+  }
+}
+
+export async function updateBudget(amount) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+    if (!user) throw new Error("User not found");
+
+    //更新指定用户的预算
+    const budget = await db.budget.upsert({
+      where: {
+        userId: user.id,
+      },
+      update: {
+        amount,
+      },
+      create: {
+        userId: user.id,
+        amount,
+      },
+    });
+
+    revalidatePath("/dashboard");
+    return {
+      success: true,
+      data: { ...budget, amount: budget.amount.toNumber() },
+    };
+  } catch (error) {
+    console.error("Error updating budget:", error);
+    return { success: false, error: error.message };
+  }
+}
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,14 +1,30 @@
+import { getCurrentBudaget } from "@/actions/budget";
 import { getUserAccounts } from "@/actions/dashboard";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import AccountCard from "./_components/account-card";
+import BudgetProgress from "./_components/budget-progress";
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
+
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudaget(defaultAccount.id);
+  }
+
   return (
     <div className="space-y-8">
       {/** Budget Progress */}
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses}
+        />
+      )}
       {/** Overview */}
       {/** Accounts Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
